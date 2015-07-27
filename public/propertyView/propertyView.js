efabf46da59995d1d4cbaa03e4ff35403ece6ace
Timothy Andrianoff <tandrianoff@gmail.com>
2015-07-27 18:57:19 -0400

    sortable table

--- public/propertyView/propertyView.js
+++ public/propertyView/propertyView.js
@@ -10,6 +10,11 @@ module.config(['$routeProvider', function($routeProvider) {
 module.controller('PropertyCtrl',['$scope','Property', function($scope, Property) {
 	$scope.test = "This is a test.";
 
+	$scope.sortType = 'sellerLastName';
+	$scope.sortReverse = false;
+
+	// Extract data from promise provided by
+	// Property service
 	Property.get().success(function(data) {
 		$scope.properties = data;
 	});
